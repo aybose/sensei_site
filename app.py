e61eaefc1faa12757a20b301b860206f4fcb9b44
Ayesha Bose <aybose@mit.edu>
2016-02-03 11:56:02 -0500

    configured login page

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def login():
     return render_template('index.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
