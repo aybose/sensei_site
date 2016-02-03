78d16c3f840318daa67134fa6373caf6c14de684
Ayesha Bose <aybose@mit.edu>
2016-02-03 11:18:20 -0500

    Changed flask files

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return "Hello prnet_site!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
