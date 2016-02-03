29eea35dd20228d2275a12451cffbe94a39a986e
Ayesha Bose <aybose@mit.edu>
2016-02-03 12:05:53 -0500

    Added home and customize pages, changed icon to wildrose

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def login():
 def home():
     return render_template('home.html')
 
+@app.route("/customize_updates")
+def customize_updates():
+    return render_template('customize_updates.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
