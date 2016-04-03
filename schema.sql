d39b12fafbe7710ae4a0aeaf6e754ac2cf49f903
Ayesha Bose <aybose@mit.edu>
2016-04-03 11:59:13 -0400

    Cleaning up files into demo folder

--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists SocialProximity;
-create table SocialProximity (
-  id integer primary key autoincrement,
-  school text not null,
-  date text not null,
-  primary_student text not null,
-  secondary_student text not null,
-  rsval integer not null
-);
